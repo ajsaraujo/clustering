71eba6a9a0d10b5dfa92f6db644840dd730bf6ad
Allan <allanjuansil@gmail.com>
2020-05-14 13:02:20 -0300

    Begin TW-k-means

--- src/generative/tw_kmeans.cpp
+++ src/generative/tw_kmeans.cpp
@@ -1,3 +1,14 @@
+#include "utils.h"
+
 int main() {
-    
+    DatasetAndFileName input = welcomeUserAndLoadData();
+
+    vector<vector<float>> elems = input.dataset;
+    int numOfObjects = elems.size();
+
+    char outputFileName[100];
+    strcpy(outputFileName, input.fileName);
+        
+    // Number of clusters that we are going to pass as an argument to K-means.
+    int numOfClusters[] = { 5, 10, 15, 20, 30 }; 
 }
