74f70eb6cd3ad0b8a5c53f0d9ac4078df10319f7
Allan <allanjuansil@gmail.com>
2020-06-03 13:45:59 -0300

    WIP

--- src/metrics/utils.py
+++ src/metrics/utils.py
@@ -1,4 +1,12 @@
-def getMatrixFromFile(fileName):
+from numpy import array
+
+def matrixToNumpyMatrix(matrix):
+    listOfArrays = [array(row) for row in matrix]
+    numpy2DArray = array(listOfArrays)
+    print(f' > dtype = {numpy2DArray.dtype}')
+    return numpy2DArray
+
+def getMatrixFromFile(fileName, transformToNumpy = True):
     with open(fileName, 'r') as file:
         rawData = file.read().split('\n')
         
@@ -8,4 +16,8 @@ def getMatrixFromFile(fileName):
         # Cast to float        
         floatMatrix = [[float(elem) for elem in line if elem != ''] for line in stringMatrix]
 
-    return floatMatrix
+    if transformToNumpy:
+        numpyMatrix = matrixToNumpyMatrix(floatMatrix)
+        return numpyMatrix
+    else:
+        return floatMatrix
